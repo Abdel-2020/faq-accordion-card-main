c8bb414289161ed934b050e3a5c598a0d74c6fad
Abdel-2020 <abdelnoura97@gmail.com>
2021-12-29 16:35:48 +0000

    29/12/2021

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 
+//Check if link is being clicked
 
-const link = document.getElementsByClassName("nav-link")[0];
+let link = document.getElementsByClassName("nav-link")[4];
 
-link.addEventListener("click", () => {
+link.addEventListener("click", function () {
     alert("clicked");
     let navText = document.getElementsByClassName("nav-text")[0];
     navText.style.display = "block";
-
-
 }) 
+
+
+link.addEventListener("mouseover", function () {
+
+    let heroBox = document.getElementsByClassName("hero-box");
+    heroBox.
+
+
+})
