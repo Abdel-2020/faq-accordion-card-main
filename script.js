a8b5bc543dcd6273eb72688450a908c66e190c09
Abdel-2020 <abdelnoura97@gmail.com>
2021-12-30 14:35:53 +0000

    Have managed to fix the click event listener.

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
 
-    //Find all nav-links and store them in "link"
-    let link = document.getElementsByClassName("nav-link");
+    //Find all nav-links and store them in "link". QuerySelectorAll stores them in an array format. 
+    const link = document.querySelectorAll(".nav-title");
 
+
+  //Create a for loop to iterate through each item in the array.
+for(let i = 0; i < link.length; i++){
+
+//Check if user has clicked the item
+    link[i].addEventListener('click', e => {
+        let text = document.getElementsByClassName("nav-text");
+        text.item(i).style.display = "block";
+        return console.log(e);
+        
+    })
+}
+ 
     //Iterate through each link and check if they've been clicked. Display the hidden text. 
-    
 
+
+
+    /*
+    for (let i = 0; i < link.length; i++) {
+        
+        link.item(i).onclick = function() {
+            let text = document.getElementsByClassName("nav-text");
+            text.item(i).style.display = "block";
+        }
+
+    }
+
+    
+/*
     for (let i = 0; i < link.length; i++) {
 
         link.item(i).addEventListener("click", function(){
             console.log("clicked");
             let text = document.getElementsByClassName("nav-text");
-            text.item(i).style.display = "block";
+           
+            return  text.item(i).style.display = "block";
         });
     }
     
+*/
+
+
 
     
 
