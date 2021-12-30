abb5a68fd5b607ff9fd10102ea5a842072e313f2
Abdel-2020 <abdelnoura97@gmail.com>
2021-12-30 12:46:02 +0000

    For loop created, console logs an alert when nav links are clicked. Does not display text yet

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 
-//Check if link is being clicked
+    //Find all nav-links and store them in "link"
+    let link = document.getElementsByClassName("nav-link");
 
-let link = document.getElementsByClassName("nav-link")[4];
+    //Iterate through each link and check if they've been clicked. Display the hidden text. 
+    
 
-link.addEventListener("click", function () {
-    alert("clicked");
-    let navText = document.getElementsByClassName("nav-text")[0];
-    navText.style.display = "block";
-}) 
+    for (let i = 0; i < link.length; i++) {
+
+        link.item(i).addEventListener("click", function(){
+            console.log("clicked");
+            let text = document.getElementsByClassName("nav-text");
+            text.item(i).style.display = "block";
+        });
+    }
+    
+
+    
 
 
-link.addEventListener("mouseover", function () {
-
-    let heroBox = document.getElementsByClassName("hero-box");
-    heroBox.
-
-
-})
